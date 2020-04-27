0fe47809dec65752117ed661dc6300927bfefd10
luis <luiscarvajal2693@gmail.com>
2020-04-27 17:06:53 -0400

    Initial version of logSystem generated by JHipster-6.8.0

---
+++ src/main/java/com/demo/lab/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.demo.lab.repository;
+
+import com.demo.lab.domain.PersistentToken;
+import com.demo.lab.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data MongoDB repository for the {@link PersistentToken} entity.
+ */
+public interface PersistentTokenRepository extends MongoRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
---
+++ src/main/java/com/demo/lab/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.demo.lab.service.dto;
